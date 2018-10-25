ae46bb58e285bcf4bd4ff317c6f631e8b78bad6b
Sebastian Templ <sebastian.templ@cern.ch>
2018-10-25 15:23:45 +0200

    Merge files

--- psgenerate.py
+++ psgenerate.py
@@ -1,7 +1,60 @@
 import argparse
 
-from psgenerate.readers import read_prescale_table, get_seeds_from_xml
-from psgenerate.writers import write_prescale_table
+
+def read_prescale_table(filepath):
+    import pandas as pd
+
+    data = pd.read_excel(filepath).to_dict('records')
+    pstable = {}
+    for entry in data:
+        pstable[entry['Name']] = {}
+        for col in entry.keys():
+            if col != 'Name':
+                pstable[entry['Name']][col] = entry[col]
+
+    colnames = []
+    for col in data[0].keys():
+        colnames.append(col)
+
+    return pstable, colnames
+
+
+def get_seeds_from_xml(filepath):
+    import xml.etree.ElementTree as ET
+
+    tree = ET.parse(filepath)
+    root = tree.getroot()
+
+    seeds = [name[0].text for name in root.findall('algorithm')]
+    indices = [int(name[2].text) for name in root.findall('algorithm')]
+
+    return seeds, indices
+
+
+def write_prescale_table(PStable, filepath='PStable_new', output_format='xlsx'):
+    import pandas as pd
+
+    if not filepath.endswith(output_format): filepath += '.' + output_format
+
+    if isinstance(PStable, dict):
+        # colnames = ['Name'].append([name for name in PStable.keys()])
+        PStable = pd.DataFrame(PStable).transpose()
+
+    PStable = PStable.fillna('-123')
+
+    int_cols = [col for col in PStable.columns if PStable[col].dtype != str]
+    for col in int_cols:
+        PStable[col] = PStable[col].astype(int)
+
+    PStable = PStable.replace(-123, '')
+
+    PStable.index.rename('Name', inplace=True)
+
+    if output_format in ['xlsx']:
+        PStable.to_excel(filepath)
+    else:
+        raise NotImplementedError('Invalid output file format: {}'.format(
+            output_format))
 
 
 def fill_empty_val(name):
