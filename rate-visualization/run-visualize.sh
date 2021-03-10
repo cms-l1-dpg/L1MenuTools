b9f5d260f507675c5a0700cef35c2e51dbb06d8a
Sebastian Templ <sebastian.templ@cern.ch>
2021-03-10 17:20:17 +0100

    Rename example PS table file, misc minor updates

--- rate-visualization/run-visualize.sh
+++ rate-visualization/run-visualize.sh
@@ -19,7 +19,7 @@ if [ ! -d "$ENV_DIR" ]; then
     python -m pip install -U pip
     python -m pip install -r requirements.txt
     deactivate
-    echo "\rCreating virtual environment...done"
+    echo "Creating virtual environment...done"
 fi
 
 . $ENV_DIR/bin/activate
