44874488000acb0383529778dc646face5bd3b4b
Sebastian Templ <sebastian.templ@cern.ch>
2021-03-10 15:43:20 +0100

    Add pstools scripts for automatic env handling

---
+++ pstools/run-ps-generate.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Run ps-generate.py in an adequate environment
+#
+# usage: bash run-ps-generate.sh <args>
+#
+
+set -e
+
+SCRIPT=ps-generate.py
+
+ENV_DIR=pstools.env
+ENV_PYTHON=python3
+
+if [ ! -d "$ENV_DIR" ]; then
+    echo "Creating virtual environment..."
+    $ENV_PYTHON -m venv $ENV_DIR
+    source $ENV_DIR/bin/activate
+    python -m pip install -U pip
+    python -m pip install -r requirements.txt
+    deactivate
+    echo "\rCreating virtual environment...done"
+fi
+
+. $ENV_DIR/bin/activate
+python $SCRIPT $@
+EXITCODE=$?
+deactivate
+
+exit $EXITCODE
