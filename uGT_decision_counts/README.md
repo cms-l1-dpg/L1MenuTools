09d72c91e0d97636487e67979381736501e7e91b
MangeshSonawane <mangesh.sonawane@cern.ch>
2022-06-27 15:17:55 +0200

    Introducing into MenuTools a script to extract the emulated trigger counts from an L1 ntuple. Inputs to the script are L1 ntuple, and an algorithm map which maps the L1 bit index to the L1 seed name. This is for validation between the trigger counts provided by the rate-estimation framework and the uGT emulated trigger counts.

---
+++ uGT_decision_counts/README.md
@@ -0,0 +1,14 @@
+## uGT decision
+
+Returns the emulated trigger counts in an ntuple for a given (L1_bit, L1_seed)  map.
+
+## algo_map
+
+Contains a list of the form [L1_bit, "L1_seed_name"], used to return the emulated counts as stored in the ntuple.
+Note : Please ensure that the mapping corresponds exactly to the indices and L1 seed names as the emulated menu, to avoid confusion. This is because the L1 ntuple does not store the L1 seed names. Inclusion of the L1 seed names in this script/map is for convenience and readability.
+
+## getEmulatorDecision.py
+
+Script that takes ntuple as input, using the provided algo map to obtain the emulated trigger counts.
+
+The output lists the L1 bit index, L1 seed name, and the triggered counts.
---
+++ uGT_decision_counts/getEmulatorDecision.py
@@ -0,0 +1,21 @@
+from ROOT import *
+from algo_map import algo_map
+
+import sys
+
+if len(sys.argv) != 2 : sys.exit("Error : Missing ntuple location\n")
+
+# Input file name
+file_in_name = sys.argv[1]
+fin = TFile(file_in_name,"READ")
+#fin = TFile("/afs/cern.ch/user/s/sonawane/private/ntuple_test/CMSSW_12_4_0_pre4/src/L1Ntuple.root","READ")
+
+#t_L1UpgradeTree = fin.Get("l1UpgradeTree/L1UpgradeTree")
+#t_l1uGTEmuTree.AddFriend("l1uGTTree/L1uGTTree")
+t_l1uGTEmuTree = fin.Get("l1uGTEmuTree/L1uGTTree")
+
+for bit, algo_name in algo_map:
+        s_algoSelection = "m_algoDecisionFinal[" + str(bit) + "]==1"
+        #print(s_algoSelection)
+        nentriesPerBit = t_l1uGTEmuTree.GetEntries(s_algoSelection)
+        print(bit, '\t', algo_name, '\t', nentriesPerBit)
