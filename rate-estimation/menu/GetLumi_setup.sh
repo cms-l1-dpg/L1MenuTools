f2c4b27ce488cc780c0cd14d5b5a7d30ffe4ab18
caruta <42809546+caruta@users.noreply.github.com>
2022-09-16 14:18:09 +0200

    Update GetLumi_setup.sh

--- rate-estimation/menu/GetLumi_setup.sh
+++ rate-estimation/menu/GetLumi_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -f json_DCSONLY.txt
-wget https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions18/13TeV/DCSOnly/json_DCSONLY.txt
+wget https://cms-service-dqmdc.web.cern.ch/CAF/certification/Collisions22/DCSOnly_JSONS/Cert_Collisions2022_355100_357900_13p6TeV_DCSOnly_TkPx.json
 export PATH=$HOME/.local/bin:/cvmfs/cms-bril.cern.ch/brilconda/bin:$PATH
