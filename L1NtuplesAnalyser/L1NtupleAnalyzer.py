5380d2460efc3b6613662be9ba04901d0deec795
elfontan <elisa.fontanesi@cern.ch>
2022-09-19 06:09:29 +0200

    Fix of the basic tools + script modified to increase the number of thresholds + HLTPhysics

--- L1NtuplesAnalyser/L1NtupleAnalyzer.py
+++ L1NtuplesAnalyser/L1NtupleAnalyzer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3                                                                                
 # ********************************                                                                                                
 # usage:                                                                                                                    
-#    python3 L1NtupleAnalyzer.py                                                                                        
+#    python3 L1NtupleAnalyzer.py
 # ********************************                                                                                              
 
 print("------> Setting Environment")
@@ -32,9 +32,9 @@ PRT_EVT  = 1000     ## Print every Nth event
 # List on ntuples:
 # ----------------
 # ZB2022 run357688 (4819637 events):
-f_ZB2022 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/2022_EphZB_run357688.csv"
+f_ZB2022 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/EphemeralZBHLTPhysics_data_run357688.csv"
 # ZB2018 run325097 (672287 events):
-f_ZB2018 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/2018_EphZB_run325097.csv"
+f_ZB2018 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/EphemeralZBHLTPhysics_data_run325097.csv"
 
 # -----------------
 # L1Ntuple branches
@@ -64,14 +64,19 @@ for f in range(len(list_ZB2022)):
 
     if f == MAX_FILE: break    
 
-nEvt_LSrange = 0
+nEvt_outLSrange = 0
 n_SingleMu22 = 0
 n_SingleMu25 = 0
 n_SingleEG36_er2p5 = 0
 n_SingleEG38_er2p5 = 0
 n_SingleEG40_er2p5 = 0
+n_DoubleLooseIsoEG22 = 0
+n_DoubleLooseIsoEG24 = 0
 n_SingleTau120_er2p1 = 0
 n_SingleTau130_er2p1 = 0
+n_DoubleIsoTau32 = 0
+n_DoubleIsoTau34 = 0
+n_DoubleIsoTau35 = 0
 n_SingleJet120er2p5 = 0
 n_SingleJet140er2p5 = 0
 n_SingleJet160er2p5 = 0
@@ -90,7 +95,7 @@ for iEvt in range(evt_tree.GetEntries()):
     
     evt_tree.GetEntry(iEvt)
     if not ((evt_tree.Event.run == 357688 and (evt_tree.Event.lumi > 35 and evt_tree.Event.lumi < 85)) or (evt_tree.Event.run == 325097 and (evt_tree.Event.lumi > 58 and evt_tree.Event.lumi < 93))):
-        nEvt_LSrange += 1
+        nEvt_outLSrange += 1
         continue
 
     L1_tree.GetEntry(iEvt)
@@ -100,54 +105,87 @@ for iEvt in range(evt_tree.GetEntries()):
     ## -----------------------------------
     preUGT_tree = L1_tree.L1Upgrade
   
-    # -----------------
+    # ----------------- 
     #        MU       #
     # -----------------
     i_mu22 = 0 # Counter to avoid to count twice an event because of two muons in the same event passing the requirement
     i_mu25 = 0 # Counter to avoid to count twice an event because of two muons in the same event passing the requirement
     for mu in range(preUGT_tree.nMuons):
-        if (preUGT_tree.muonEt[mu] > 22 and i_mu22 == 0): #muonEt = 0.5*muonIEt
+        if (preUGT_tree.muonEt[mu] >= 22 and preUGT_tree.muonQual[mu] >= 12 and preUGT_tree.muonBx[mu] == 0 and i_mu22 == 0): #muonEt = 0.5*muonIEt
             i_mu22 += 1
             n_SingleMu22 += 1
             #print("muonEt = ", preUGT_tree.muonEt[mu])
             #print("muonIEt = ", preUGT_tree.muonIEt[mu])
-        if (preUGT_tree.muonEt[mu] > 25 and i_mu25 == 0): #muonEt = 0.5*muonIEt
+        if (preUGT_tree.muonEt[mu] >= 25 and preUGT_tree.muonQual[mu] >= 12 and preUGT_tree.muonBx[mu] == 0 and i_mu25 == 0): #muonEt = 0.5*muonIEt
             i_mu25 += 1
             n_SingleMu25 += 1
 
     # -----------------
     #        EG       #
     # -----------------
+    i_isoeg22 = 0 # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
+    i_isoeg24 = 0 # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
     i_eg36 = 0 # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
     i_eg38 = 0 # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
     i_eg40 = 0 # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
     for eg in range(preUGT_tree.nEGs):
-        if (preUGT_tree.egEt[eg] > 36 and abs(preUGT_tree.egEta[eg]) < 2.5 and i_eg36 == 0): #egEt = 0.5*egIEt
+
+        if (preUGT_tree.egEt[eg] >= 36 and abs(preUGT_tree.egEta[eg]) <= 2.5 and preUGT_tree.egBx[eg] == 0 and i_eg36 == 0): #egEt = 0.5*egIEt
             i_eg36 += 1
             n_SingleEG36_er2p5 += 1
             #print("egEt = ", preUGT_tree.egEt[eg])
             #print("egIEt = ", preUGT_tree.egIEt[eg])
-        if (preUGT_tree.egEt[eg] > 38 and abs(preUGT_tree.egEta[eg]) < 2.5 and i_eg38 == 0): #egEt = 0.5*egIEt
+        if (preUGT_tree.egEt[eg] >= 38 and abs(preUGT_tree.egEta[eg]) <= 2.5 and preUGT_tree.egBx[eg] == 0 and i_eg38 == 0): #egEt = 0.5*egIEt
             i_eg38 += 1
             n_SingleEG38_er2p5 += 1
-        if (preUGT_tree.egEt[eg] > 40 and abs(preUGT_tree.egEta[eg]) < 2.5 and i_eg40 == 0): #egEt = 0.5*egIEt
+        if (preUGT_tree.egEt[eg] >= 40 and abs(preUGT_tree.egEta[eg]) <= 2.5 and preUGT_tree.egBx[eg] == 0 and i_eg40 == 0): #egEt = 0.5*egIEt
             i_eg40 += 1
             n_SingleEG40_er2p5 += 1
+
+        if (preUGT_tree.egEt[eg] >= 22 and abs(preUGT_tree.egEta[eg]) <= 2.1 and (preUGT_tree.egIso[eg] == 2 or preUGT_tree.egIso[eg] == 3) and preUGT_tree.egBx[eg] == 0 and i_isoeg22 == 0): #tauEt = 0.5*tauIEt
+            for eg2 in range(preUGT_tree.nEGs):
+                if ((not eg2 == eg) and preUGT_tree.egEt[eg2] >= 22 and abs(preUGT_tree.egEta[eg2]) <= 2.1 and (preUGT_tree.egIso[eg2] == 2 or preUGT_tree.egIso[eg2] == 3) and preUGT_tree.egBx[eg2] == 0): #tauEt = 0.5*tauIEt
+                    i_isoeg22 += 1
+                    n_DoubleLooseIsoEG22 += 1
+        if (preUGT_tree.egEt[eg] >=  24 and abs(preUGT_tree.egEta[eg]) <= 2.1 and (preUGT_tree.egIso[eg] == 2 or preUGT_tree.egIso[eg] == 3) and preUGT_tree.egBx[eg] == 0 and i_isoeg24 == 0): #tauEt = 0.5*tauIEt
+            for eg2 in range(preUGT_tree.nEGs):
+                if ((not eg2 == eg) and preUGT_tree.egEt[eg2] >= 24 and abs(preUGT_tree.egEta[eg2]) <= 2.1 and (preUGT_tree.egIso[eg2] == 2 or preUGT_tree.egIso[eg2] == 3) and preUGT_tree.egBx[eg2] == 0): #tauEt = 0.5*tauIEt
+                    i_isoeg24 += 1
+                    n_DoubleLooseIsoEG24 += 1
 
     # -----------------
     #       TAU       #
     # -----------------
+    i_tau32 = 0 # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
+    i_tau34 = 0 # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
+    i_tau35 = 0 # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
     i_tau120 = 0 # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
     i_tau130 = 0 # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
     for tau in range(preUGT_tree.nTaus):
-        if (preUGT_tree.tauEt[tau] > 120 and abs(preUGT_tree.tauEta[tau]) < 2.1 and i_tau120 == 0): #tauEt = 0.5*tauIEt
+        if (preUGT_tree.tauEt[tau] >= 120 and abs(preUGT_tree.tauEta[tau]) <= 2.1 and preUGT_tree.tauBx[tau] == 0 and i_tau120 == 0): #tauEt = 0.5*tauIEt
             i_tau120 += 1
             n_SingleTau120_er2p1 += 1
             #print("tauEt = ", preUGT_tree.tauEt[tau])
             #print("tauIEt = ", preUGT_tree.tauIEt[tau])
-        if (preUGT_tree.tauEt[tau] > 130 and abs(preUGT_tree.tauEta[tau]) < 2.1 and i_tau130 == 0): #tauEt = 0.5*tauIEt
+        if (preUGT_tree.tauEt[tau] >= 130 and abs(preUGT_tree.tauEta[tau]) <= 2.1 and preUGT_tree.tauBx[tau] == 0 and i_tau130 == 0): #tauEt = 0.5*tauIEt
             i_tau130 += 1
             n_SingleTau130_er2p1 += 1
+
+        if (preUGT_tree.tauEt[tau] >= 32 and preUGT_tree.tauIso[tau] == 1 and preUGT_tree.tauBx[tau] == 0 and i_tau32 == 0): #tauEt = 0.5*tauIEt
+            for tau2 in range(preUGT_tree.nTaus):
+                if ((not tau2 == tau) and preUGT_tree.tauEt[tau2] >= 32 and preUGT_tree.tauIso[tau2] == 1 and preUGT_tree.tauBx[tau2] == 0):
+                    i_tau32 += 1
+                    n_DoubleIsoTau32 += 1
+        if (preUGT_tree.tauEt[tau] >= 34 and preUGT_tree.tauIso[tau] == 1 and preUGT_tree.tauBx[tau] == 0 and i_tau34 == 0): #tauEt = 0.5*tauIEt
+            for tau2 in range(preUGT_tree.nTaus):
+                if ((not tau2 == tau) and preUGT_tree.tauEt[tau2] >= 34 and preUGT_tree.tauIso[tau2] == 1 and preUGT_tree.tauBx[tau2] == 0):
+                    i_tau34 += 1
+                    n_DoubleIsoTau34 += 1
+        if (preUGT_tree.tauEt[tau] >= 35 and preUGT_tree.tauIso[tau] == 1 and preUGT_tree.tauBx[tau] == 0 and i_tau35 == 0): #tauEt = 0.5*tauIEt
+            for tau2 in range(preUGT_tree.nTaus):
+                if ((not tau2 == tau) and preUGT_tree.tauEt[tau2] >= 35 and preUGT_tree.tauIso[tau2] == 1 and preUGT_tree.tauBx[tau2] == 0):
+                    i_tau35 += 1
+                    n_DoubleIsoTau35 += 1
 
     # -----------------
     #       JET       #
@@ -157,18 +195,18 @@ for iEvt in range(evt_tree.GetEntries()):
     i_jet160 = 0 # Counter to avoid to count twice an event because of two jets in the same event passing the requirement
     i_jet180 = 0 # Counter to avoid to count twice an event because of two jets in the same event passing the requirement
     for jet in range(preUGT_tree.nJets):
-        if (preUGT_tree.jetEt[jet] > 120 and abs(preUGT_tree.jetEta[jet]) < 2.5 and i_jet120 == 0): #jetEt = 0.5*tauIEt
+        if (preUGT_tree.jetEt[jet] >= 120 and abs(preUGT_tree.jetEta[jet]) <= 2.5 and preUGT_tree.jetBx[jet] == 0 and i_jet120 == 0): #jetEt = 0.5*tauIEt
             i_jet120 += 1
             n_SingleJet120er2p5 += 1
             #print("jetEt = ", preUGT_tree.jetEt[jet])
             #print("jetIEt = ", preUGT_tree.jetIEt[jet])
-        if (preUGT_tree.jetEt[jet] > 140 and abs(preUGT_tree.jetEta[jet]) < 2.5 and i_jet140 == 0): #jetEt = 0.5*tauIEt
+        if (preUGT_tree.jetEt[jet] >= 140 and abs(preUGT_tree.jetEta[jet]) <= 2.5 and preUGT_tree.jetBx[jet] == 0 and i_jet140 == 0): #jetEt = 0.5*tauIEt
             i_jet140 += 1
             n_SingleJet140er2p5 += 1
-        if (preUGT_tree.jetEt[jet] > 160 and abs(preUGT_tree.jetEta[jet]) < 2.5 and i_jet160 == 0): #jetEt = 0.5*tauIEt
+        if (preUGT_tree.jetEt[jet] >= 160 and abs(preUGT_tree.jetEta[jet]) <= 2.5 and preUGT_tree.jetBx[jet] == 0 and i_jet160 == 0): #jetEt = 0.5*tauIEt
             i_jet160 += 1
             n_SingleJet160er2p5 += 1
-        if (preUGT_tree.jetEt[jet] > 180 and abs(preUGT_tree.jetEta[jet]) < 2.5 and i_jet180 == 0): #jetEt = 0.5*tauIEt
+        if (preUGT_tree.jetEt[jet] >= 180 and abs(preUGT_tree.jetEta[jet]) <= 2.5 and preUGT_tree.jetBx[jet] == 0 and i_jet180 == 0): #jetEt = 0.5*tauIEt
             i_jet180 += 1
             n_SingleJet180er2p5 += 1
 
@@ -181,19 +219,19 @@ for iEvt in range(evt_tree.GetEntries()):
     i_etmhf100 = 0 # Counter to avoid to count twice an event because of two met values in the same event passing the requirement
 
     for met in range(preUGT_tree.nSums):
-        if (not(preUGT_tree.sumType[met] == 'kMissingEtHF')): continue
-        if (preUGT_tree.sumEt[met] > 70 and i_etmhf70 == 0): #sumEt = 0.5*sumIEt
+        if (not(preUGT_tree.sumType[met] == 8)): continue # 8 = 'kMissingEtHF'
+        if (preUGT_tree.sumEt[met] >= 70 and preUGT_tree.sumBx[met] == 0 and i_etmhf70 == 0): #sumEt = 0.5*sumIEt
             i_etmhf70 += 1
             n_ETMHF70 += 1
             #print("sumEt = ", preUGT_tree.sumEt[met])
             #print("sumIEt = ", preUGT_tree.sumIEt[met])
-        if (preUGT_tree.sumEt[met] > 80 and i_etmhf80 == 0): #sumEt = 0.5*sumIEt
+        if (preUGT_tree.sumEt[met] >= 80 and preUGT_tree.sumBx[met] == 0 and i_etmhf80 == 0): #sumEt = 0.5*sumIEt
             i_etmhf80 += 1
             n_ETMHF80 += 1
-        if (preUGT_tree.sumEt[met] > 90 and i_etmhf90 == 0): #sumEt = 0.5*sumIEt
+        if (preUGT_tree.sumEt[met] >= 90 and preUGT_tree.sumBx[met] == 0 and i_etmhf90 == 0): #sumEt = 0.5*sumIEt
             i_etmhf90 += 1
             n_ETMHF90 += 1
-        if (preUGT_tree.sumEt[met] > 100 and i_etmhf100 == 0): #sumEt = 0.5*sumIEt
+        if (preUGT_tree.sumEt[met] >= 100 and preUGT_tree.sumBx[met] == 0 and i_etmhf100 == 0): #sumEt = 0.5*sumIEt
             i_etmhf100 += 1
             n_ETMHF100 += 1
 
@@ -205,9 +243,14 @@ print("###### EG ######")
 print("n_SingleEG36_er2p5 = ", n_SingleEG36_er2p5)
 print("n_SingleEG38_er2p5 = ", n_SingleEG38_er2p5)
 print("n_SingleEG40_er2p5 = ", n_SingleEG40_er2p5)
+print("n_DoubleLooseIsoEG22 = ", n_DoubleLooseIsoEG22)
+print("n_DoubleLooseIsoEG24 = ", n_DoubleLooseIsoEG24)
 print("###### TAU ######")
 print("n_SingleTau120_er2p1 = ", n_SingleTau120_er2p1)
 print("n_SingleTau130_er2p1 = ", n_SingleTau130_er2p1)
+print(" n_DoubleIsoTau32 = ", n_DoubleIsoTau32)
+print(" n_DoubleIsoTau34 = ", n_DoubleIsoTau34)
+print(" n_DoubleIsoTau35 = ", n_DoubleIsoTau35)
 print("###### JET ######")
 print("n_SingleJet120er2p5 = ", n_SingleJet120er2p5)
 print("n_SingleJet140er2p5 = ", n_SingleJet140er2p5)
@@ -219,11 +262,9 @@ print("n_ETMHF80 = ", n_ETMHF80)
 print("n_ETMHF90 = ", n_ETMHF90)
 print("n_ETMHF100 = ", n_ETMHF100)
 print("###### ... ######")
-print(" = ", )
 
 
 print("--------------------------")
 print("FINAL Nentries evt_tree: ", evt_tree.GetEntries())
-print("FINAL Nentries preUGT_tree: ", preUGT_tree.GetEntries())
-print("FINAL Nentries evt_tree in LS range: ", nEvt_LSrange)
+print("FINAL Nentries evt_tree in LS range: ", (evt_tree.GetEntries() - nEvt_outLSrange))
 print("--------------------------")
---
+++ L1NtuplesAnalyser/L1NtupleAnalyzer_functionOfPt.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3                                                                                
+# ********************************                                                                                                
+# usage:                                                                                                                    
+#    python3 L1NtupleAnalyzer_functionOfPt.py  
+# ********************************                                                                                              
+
+print("------> Setting Environment")
+
+import sys
+import math
+from subprocess import Popen,PIPE
+from ROOT import *
+import numpy as np
+from array import *
+import ROOT, array, random, copy
+from ROOT import TCanvas, TFile, TH1, TH1F, TF1, gSystem
+
+ROOT.gROOT.SetBatch()
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptTitle(0)
+gSystem.Load('libRooFit')
+
+# ----------------------
+# Configuration settings
+# ----------------------
+USE_EMUL = False    ## Use emulated L1T muons instead of unpacked
+MAX_FILE = -1       ## Maximum number of input files (use "-1" for unlimited)
+MAX_EVT  = -1       ## Maximum number of events to process (use "-1" for ALL)
+PRT_EVT  = 1000     ## Print every Nth event
+
+# ----------------
+# List on ntuples:
+# ----------------
+# ZB2022 run357688 (4819637 events):
+f_ZB2022 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/EphemeralZBHLTPhysics_data_run357688.csv"
+# ZB2018 run325097 (672287 events):
+f_ZB2018 = "/afs/cern.ch/work/e/elfontan/private/L1TMenu/13p6TeVcollisions_ntupleProduction/CMSSW_12_4_0/src/L1MenuTools/rate-estimation/ntuple/EphemeralZBHLTPhysics_data_run325097.csv"
+
+# -----------------
+# L1Ntuple branches
+# -----------------
+evt_tree  = TChain('l1EventTree/L1EventTree')
+if not USE_EMUL:
+    L1_tree = TChain('l1UpgradeTree/L1UpgradeTree')
+else:
+    L1_tree = TChain('l1UpgradeEmuTree/L1UpgradeTree')
+
+list_ZB2022 = []
+list_ZB2018 = []
+
+with open(f_ZB2022) as file:
+    while (line := file.readline().rstrip()):
+        list_ZB2022.append(line)
+
+with open(f_ZB2018) as file:
+    while (line := file.readline().rstrip()):
+        list_ZB2018.append(line)
+
+  
+for f in range(len(list_ZB2018)):
+    evt_tree.Add(list_ZB2018[f])
+    #print("INTERMEDIATE Nentries evt_tree: ", evt_tree.GetEntries())
+    L1_tree.Add(list_ZB2018[f])
+
+    if f == MAX_FILE: break    
+
+nEvt_outLSrange = 0
+
+n_SingleMu_thr = [0, 0, 0, 0, 0, 0, 0, 0] #8 
+mu_pt = [20, 21, 22, 23, 24, 25, 26, 27] 
+
+n_SingleEG_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] #9
+eg_pt = [32, 33, 34, 35, 36, 37, 38, 39, 40]
+
+n_DoubleLooseIsoEG_thr = [0, 0, 0, 0, 0, 0, 0, 0] #8
+doubleeg_pt = [20, 21, 22, 23, 24, 25, 26]
+
+n_SingleTau_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #11
+tau_pt = [120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130]
+
+n_DoubleIsoTau_thr = [0, 0, 0, 0, 0, 0, 0] #7
+doubleisotau_pt = [30, 31, 32, 33, 34, 35, 36]
+
+n_SingleJet_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] #9
+jet_pt = [120, 125, 130, 135, 140, 145, 150, 155, 160]
+
+n_ETMHF_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] #9
+etmhf_pt = [70, 72, 75, 78, 80, 85, 90, 95, 100]
+
+# -------------------------
+# Loop over events in TFile
+# -------------------------
+for iEvt in range(evt_tree.GetEntries()):
+    if MAX_EVT > 0 and iEvt > MAX_EVT: break
+    if iEvt % PRT_EVT is 0: print("Event #", iEvt)
+    
+    evt_tree.GetEntry(iEvt)
+    if not ((evt_tree.Event.run == 357688 and (evt_tree.Event.lumi > 35 and evt_tree.Event.lumi < 85)) or (evt_tree.Event.run == 325097 and (evt_tree.Event.lumi > 58 and evt_tree.Event.lumi < 93))):
+        nEvt_outLSrange += 1
+        continue
+
+    L1_tree.GetEntry(iEvt)
+
+    ## -----------------------------------
+    ## pre UGT tree: muon and calo objects
+    ## -----------------------------------
+    preUGT_tree = L1_tree.L1Upgrade
+  
+    # ----------------- 
+    #        MU       #
+    # -----------------
+    i_mu_thr = [0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two mu objects in the same event passing the requirement
+    for mu in range(preUGT_tree.nMuons):
+        for thr in range(len(mu_pt)):
+            if (preUGT_tree.muonEt[mu] >= mu_pt[thr] and preUGT_tree.muonQual[mu] >= 12 and preUGT_tree.muonBx[mu] == 0 and i_mu_thr[thr] == 0): #muonEt = 0.5*muonIEt
+                i_mu_thr[thr] += 1
+                n_SingleMu_thr[thr] += 1
+
+    # -----------------
+    #        EG       #
+    # -----------------
+    i_eg_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
+    i_doubleeg_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two eg objects in the same event passing the requirement
+    for eg in range(preUGT_tree.nEGs):
+        for thr in range(len(eg_pt)):
+            if (preUGT_tree.egEt[eg] >= eg_pt[thr] and abs(preUGT_tree.egEta[eg]) <= 2.5 and preUGT_tree.egBx[eg] == 0 and i_eg_thr[thr] == 0): #egEt = 0.5*egIEt
+                i_eg_thr[thr] += 1
+                n_SingleEG_thr[thr] += 1
+
+        for thr in range(len(doubleeg_pt)):
+            if (preUGT_tree.egEt[eg] >= doubleeg_pt[thr] and abs(preUGT_tree.egEta[eg]) <= 2.1 and (preUGT_tree.egIso[eg] == 2 or preUGT_tree.egIso[eg] == 3) and preUGT_tree.egBx[eg] == 0 and i_doubleeg_thr == 0): #egEt = 0.5*egIEt
+                for eg2 in range(preUGT_tree.nEGs):
+                    if ((not eg2 == eg) and preUGT_tree.egEt[eg2] >= doubleeg_pt[thr] and abs(preUGT_tree.egEta[eg2]) <= 2.1 and (preUGT_tree.egIso[eg2] == 2 or preUGT_tree.egIso[eg2] == 3) and preUGT_tree.egBx[eg2] == 0): #egEt = 0.5*egIEt
+                        i_doubleeg_thr[thr] += 1
+                        n_DoubleLooseIsoEG_thr[thr] += 1
+
+
+    # -----------------
+    #       TAU       #
+    # -----------------
+    i_tau_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
+    i_doubleisotau_thr = [0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two taus in the same event passing the requirement
+    for tau in range(preUGT_tree.nTaus):
+        for thr in range(len(tau_pt)):
+            if (preUGT_tree.tauEt[tau] >= tau_pt[thr] and abs(preUGT_tree.tauEta[tau]) <= 2.1 and preUGT_tree.tauBx[tau] == 0 and i_tau_thr[thr] == 0): #tauEt = 0.5*tauIEt
+                i_tau_thr[thr] += 1
+                n_SingleTau_thr[thr] += 1
+
+        for thr in range(len(doubleisotau_pt)):
+            if (preUGT_tree.tauEt[tau] >= doubleisotau_pt[thr] and preUGT_tree.tauIso[tau] == 1 and preUGT_tree.tauBx[tau] == 0 and i_doubleisotau_thr[thr] == 0): #tauEt = 0.5*tauIEt
+                for tau2 in range(preUGT_tree.nTaus):
+                    if ((not tau2 == tau) and preUGT_tree.tauEt[tau2] >= doubleisotau_pt[thr] and preUGT_tree.tauIso[tau2] == 1 and preUGT_tree.tauBx[tau2] == 0):
+                        i_doubleisotau_thr[thr] += 1
+                        n_DoubleIsoTau_thr[thr] += 1
+
+
+    # -----------------
+    #       JET       #
+    # -----------------
+    i_jet_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two jets in the same event passing the requirement
+    for jet in range(preUGT_tree.nJets):
+        for thr in range(len(jet_pt)):
+            if (preUGT_tree.jetEt[jet] >= jet_pt[thr] and abs(preUGT_tree.jetEta[jet]) <= 2.5 and preUGT_tree.jetBx[jet] == 0 and i_jet_thr[thr] == 0): #jetEt = 0.5*tauIEt
+                i_jet_thr[thr] += 1
+                n_SingleJet_thr[thr] += 1
+
+
+    # -----------------
+    #       MET       #
+    # -----------------
+    i_etmhf_thr = [0, 0, 0, 0, 0, 0, 0, 0, 0] # Counter to avoid to count twice an event because of two met values in the same event passing the requirement
+    for met in range(preUGT_tree.nSums):
+        for thr in range(len(etmhf_pt)):
+            if (not(preUGT_tree.sumType[met] == 8)): continue # 8 = 'kMissingEtHF'
+            if (preUGT_tree.sumEt[met] >= etmhf_pt[thr] and preUGT_tree.sumBx[met] == 0 and i_etmhf_thr[thr] == 0): #sumEt = 0.5*sumIEt
+                i_etmhf_thr[thr] += 1
+                n_ETMHF_thr[thr] += 1
+
+
+print("-------------------------")
+print("-------- SUMMARY --------")
+print("-------------------------")
+print("FINAL Nentries evt_tree: ", evt_tree.GetEntries())
+print("FINAL Nentries evt_tree in LS range: ", (evt_tree.GetEntries() - nEvt_outLSrange))
+print("-------------------------")
+
+print("# MU #")
+print("mu pt thresholds = ", mu_pt)
+print("n_SingleMu_Countings = ", n_SingleMu_thr)
+
+print("# EG #")
+print("eg pt thresholds = ", eg_pt)
+print("n_SingleEG_Countings = ", n_SingleEG_thr)
+print("doubleeg pt thresholds = ", doubleeg_pt)
+print("n_DoubleLooseIsoEG_Countings = ", n_DoubleLooseIsoEG_thr)
+
+print("# TAU #")
+print("tau pt thresholds = ", tau_pt)
+print("n_SingleTau_Countings = ", n_SingleTau_thr)
+print("doubleisotau pt thresholds = ", doubleisotau_pt)
+print("n_DoubleIsoTau_Countings = ", n_DoubleIsoTau_thr)
+
+print("# JET #")
+print("jet pt thresholds = ", jet_pt)
+print("n_SingleJet_Countings = ", n_SingleJet_thr)
+
+print("# MET #")
+print("met thresholds = ", etmhf_pt)
+print("n_ETMHF_Countings = ", n_ETMHF_thr)
