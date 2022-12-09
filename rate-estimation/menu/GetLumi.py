409c5b4d0fbcfa07e03da3c03655b9fb18fd8dfc
Elisa Fontanesi <31408151+elfontan@users.noreply.github.com>
2022-12-09 12:21:05 +0100

    Script adapted to python3 and new brilcalc

--- rate-estimation/menu/GetLumi.py
+++ rate-estimation/menu/GetLumi.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 # File        : GetLumi.py
@@ -11,26 +11,18 @@
 import subprocess
 import pandas as pd
 import sys
-if sys.version_info[0] < 3:
-    from StringIO import StringIO
-else:
-    from io import StringIO
+import io
 
 runlist = [
-
-#	two runs in Fill 8124
-	357440,
-	357442
+    # High PU fill                                                                                                                                                                         
+    362433, 362434, 362435, 362436, 362437, 362438, 362439
 ]
 
 def Runcmd(run):
-    # cmd = "brilcalc lumi --byls"
     cmd = "brilcalc lumi --byls -u '1e30/cm2s' "
     cmd += " --output-style csv -b 'STABLE BEAMS' "
     cmd += " -r %d " % run
-    #testcmd = cmd + " --normtag /afs/cern.ch/user/l/lumipro/public/normtag_file/normtag_DATACERT.json "
-    #testcmd = cmd + " -i /afs/cern.ch/user/d/deguio/public/Certification/Cert_13TeV_2017_HCAL_DCS_GOOD.txt "
-    testcmd = cmd + " -i Cert_Collisions2022_355100_357900_13p6TeV_DCSOnly_TkPx.json "
+    testcmd = cmd + " -i Cert_Collisions2022_355100_362760_eraBCDEFG_13p6TeV_DCSOnly_TkPx.json "
     pipe = subprocess.Popen(testcmd, shell=True, stdout=subprocess.PIPE)
     out, err = pipe.communicate()
     if len(out) == 221: # Empty output
@@ -39,16 +31,12 @@ def Runcmd(run):
     Pandas(out)
 
 def Pandas(file):
-    # df = pd.read_csv(file, header=1, skipfooter=3)
-    # df = pd.read_csv(StringIO(file), header=1, skipfooter=3)
-    df = pd.read_csv(StringIO(file), header=1, skipfooter=3, engine='python')
+    df = pd.read_csv(io.BytesIO(file), header=1, skipfooter=3, engine='python')
     df.loc[:,"run"] = df["#run:fill"].str.split(':').str[0]
     df.loc[:,"fill"] = df["#run:fill"].str.split(':').str[1]
     df.loc[:,"LS"] = df["ls"].str.split(':').str[0]
     df2=df[['fill','run','LS', 'avgpu']]
     df2.to_csv('run_lumi.csv', mode='a', header=False, index=False)
-    # df2.to_csv('run_lumi_old.csv', mode='a', header=False, index=False)
-    # df.to_csv('lumi_new.csv', mode='a', header=False, index=False)
 
 if __name__ == "__main__":
     for run in runlist:
